fe354ab45bfbcc62e551912321ea9f72cdc4b16b
christzelep <trendinggadgets23@gmail.com>
2025-06-23 13:21:17 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,8 @@
-const CACHE_NAME = 'woodwizard-v1';
+const CACHE_NAME = 'woodwizard-v2';
 const urlsToCache = [
   '/',
   'index.html',
-  'styles.css',
-  'main.js',
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
   'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
   'icons/icon-512x512.png'
 ];
 
@@ -18,7 +10,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Cache opened');
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -31,29 +23,13 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
+        return fetch(event.request);
       })
   );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
-  
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
